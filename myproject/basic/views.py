d143bcfc8efc36fe331f7956ea37fc3dcd779998
anisettichandu <anisettichandu9398@gmail.comgit config --listgit config --global user.email anisettichandu9398@gmail.com>
2025-11-06 23:33:06 +0530

    upda

--- myproject/basic/views.py
+++ myproject/basic/views.py
@@ -5,9 +5,18 @@ from django.http import HttpResponse
 def fun(requests):
     return HttpResponse("hello chandu")
 def fun1(requests):
+    choice=requests.Get.get("choice",'')
     a=requests.GET.get("a",'0')
     b=requests.GET.get("b",'0')
-    c=int(a)+int(b)
-    return HttpResponse("you choose the option 1")
-    return HttpResponse(f"sum of given two numbers is {c}")
+    if int(choice)==1:
+        c=int(a)+int(b)
+        return HttpResponse("you choose the option 1 \n so addition is {c}")
+    elif choice==2:
+        c=int(a)-int(b)
+        return HttpResponse("you choose the option 2 \n so subtraction is {c}")
+    else:
+        return HttpResponse("enter the valid choice")
+        
+        
+    
 # Create your views here.
