5d0904f173d04ec4ccff42e3eac0c0c08c36189b
anisettichandu <anisettichandu9398@gmail.comgit config --listgit config --global user.email anisettichandu9398@gmail.com>
2025-11-07 10:43:33 +0530

    change

--- myproject/basic/views.py
+++ myproject/basic/views.py
@@ -8,12 +8,17 @@ def fun1(requests):
     choice=requests.GET.get("choice",'')
     a=requests.GET.get("a",'0')
     b=requests.GET.get("b",'0')
+    if choice==int(choice):
+        return HttpResponse("choice=1 (addition) \n choice=2 (subtraction) \n choice=3 (Multiplication)",content_type="text/plane")
     if int(choice)==1:
         c =int(a)+int(b)
         return HttpResponse(f"you choose the option 1 \n so addition is {c}", content_type="text/plane")
     elif int(choice)==2:
         c=int(a)-int(b)
         return HttpResponse(f"you choose the option 2 \n so subtraction is {c}",content_type="text/plane")
+    elif int(choice==3):
+        c=int(a)*int(b)
+        return HttpResponse(f"you choose the option 3 \n so mulitiplace is {c}",content_type="text/plane")
     else:
         return HttpResponse(f"enter the valid choice")
         
