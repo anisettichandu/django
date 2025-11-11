ae50794b0d7c7c137f80b03e671957ce20fe27a1
anisettichandu <anisettichandu9398@gmail.comgit config --listgit config --global user.email anisettichandu9398@gmail.com>
2025-11-11 23:11:40 +0530

    update

--- myproject/basic/models.py
+++ myproject/basic/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField(unique=True)
